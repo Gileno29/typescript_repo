247f2d430d39dd7f0221292aee749dbf8bc2466b
Gileno <gileno.cr.duarte@gmail.com>
2024-10-17 15:00:03 -0300

    exemplo ade de class decorator

--- src/decorators.ts
+++ src/decorators.ts
@@ -5,3 +5,19 @@ function ExibirNome(target: any){
 
 @ExibirNome
 class Funcionario{} 
+
+// Class Decorator
+
+function apiVersion(version:string){
+    return (target: any) =>{
+        Object.assign(target.prototype,{__version: version})
+    }
+}
+
+@apiVersion("1.10")
+class Api{
+
+}
+
+const api = new Api();
+console.log(api.__version);
