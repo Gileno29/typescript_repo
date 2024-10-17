ea5e0d1ae3280d7cd811fa443977f258752cc567
Gileno <gileno.cr.duarte@gmail.com>
2024-10-17 08:45:01 -0300

    utilizando generic

--- src/generics.ts
+++ src/generics.ts
@@ -8,7 +8,7 @@ function concatArray<T>(...itens: T[]):T[]{
 
 const numArray = concatArray<number[]>([1,5], [3])
 
-const stringArray = concatArray<string[]>(["felipe", "goku"],["vegeta"]);
+const stringArray = concatArray<string[]>(["saitama", "goku"],["vegeta"]);
 
 console.log(numArray)
 console.log(stringArray)
